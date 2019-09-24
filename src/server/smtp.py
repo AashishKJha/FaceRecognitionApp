89881a1cff8f6c6256bb339aeecf3f71d602fa96
Aashish K Jha <30407576+UnluckyAj@users.noreply.github.com>
2019-09-24 10:51:02 +0530

    Update smtp.py

--- src/server/smtp.py
+++ src/server/smtp.py
@@ -2,7 +2,7 @@ import smtplib
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("aashishjha.1994@gmail.com", "aashishgolu")
+server.login("aashishjha.1994@gmail.com", "")
  
 msg = "Hello!"
 server.sendmail("aashishjha.1994@gmail.com", "adityanew2014@gmail.com", msg)
